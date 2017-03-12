1589cf890109ba336c6ca290afee1be30d64f54a
tygruv-git <halamkovakl@gmail.com>
2017-03-12 13:07:20 +0100

    init

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang='cs'>
+  <head>
+    <title>TODO list</title>
+    <meta charset='utf-8'>
+    <style>
+      
+    </style>
+  </head>
+  <body>
+    <h1>Ahoj kláro</h1> 
+  </body>
+</html>
